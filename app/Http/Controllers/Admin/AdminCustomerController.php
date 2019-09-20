200a881af6504b64a70fb72e2a3074098149521c
yikang <1115610574@qq.com>
2019-09-20 14:43:28 +0800

    down controller and create request

--- app/Http/Controllers/Admin/AdminCustomerController.php
+++ app/Http/Controllers/Admin/AdminCustomerController.php
@@ -4,24 +4,64 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminCustomerRequest;
+use App\Models\User;
 
 class AdminCustomerController extends Controller
 {
     public function getAllCustomersByCompanyId(Request $request)
     {
-        return 'hello';
+        $id = $request->get('company_id');
+        $data = User::where('type', $id)->paginate(8);
+        $result = ['code' => '200', 'msg' => '成功', 'data' => $data];
+        return response()->json($result);
     }
 
     public function getCustomerInfoByCustomerId(Request $request)
     {
-        return 'hello';
+        $id = $request->get('customer_id');
+        $data = User::where('id', $id)->first();
+        if ($data) {
+            $result = ['code' => '200', 'msg' => '成功', 'data' => [
+                'customer_id' => $data->id,
+                'customer_name' => $data->cname,
+                'customer__phone' => $data->tel,
+                'customer_create_time' => $data->created_at,
+                'customer_state' => $data->status,
+                'customer_commpany' => $data->type,
+                'customer_other_info' => null
+            ]];
+        } else {
+            $result = ['code' => '100', 'msg' => '失败', 'data' => null];
+        }
+        return response()->json($result);
     }
-    public function addCustomer(Request $request)
+    public function addCustomer(AdminCustomerRequest $request)
     {
-        return 'hello';
+        $customer = new User();
+        $customer->cname = $request->get('customer_name');
+        $customer->password = bcrypt($request->get('customer_password'));
+        $customer->type = $request->get('customer_company_id');
+        $customer->tel = $request->get('customer_phone');
+        $customer->created_at = date('Y-m-d h:i:s', time());
+        $result = $customer->save();
+
+        if ($result) {
+            $result = ['code' => '200', 'msg' => '成功', 'data' => null];
+        } else {
+            $result = ['code' => '100', 'msg' => '失败', 'data' => null];
+        }
+        return response()->json($result);
     }
     public function deleteCustomer(Request $request)
     {
-        return 'hello';
+        $id = $request->get('customer_id');
+        $vis = User::where('id', $id)->delete();
+        if ($vis) {
+            $result = ['code' => '200', 'msg' => '成功', 'data' => null];
+        } else {
+            $result = ['code' => '100', 'msg' => '失败', 'data' => null];
+        }
+        return response()->json($result);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,6 @@ class User extends Model
 {
     //关联的数据表
     protected $table = 'chat_user';
-
+    //关闭时间戳
     public $timestamps = false;
 }
